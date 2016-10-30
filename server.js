204941a3213d08689718fecafc40d6bf83e4b317
Darshan <dave1089@live.com>
2016-10-30 06:36:21 +0000

    multer added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express")
+var multer  = require('multer')
+var upload = multer()// multer({ dest: 'uploads/' })
 var app = express();
-var mongo = require("mongodb").MongoClient
-var link = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'  && 
+// var mongo = require("mongodb").MongoClient
+// var link = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'  && 
 var port = process.env.PORT || 8080;
-
+// Handling Home url
+app.use(express.static(__dirname + '/public'))
 
 
 
