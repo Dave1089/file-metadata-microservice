03241464f36d5ddfdb5edc3bd478504af4f1c766
Darshan <dave1089@live.com>
2016-10-30 18:59:28 +0000

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var multer  = require('multer')
-var upload = multer()// multer({ dest: 'uploads/' })
+var upload = multer({ dest: './uploads/' }) 
 var app = express();
 // var mongo = require("mongodb").MongoClient
 // var link = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'  && 
@@ -8,6 +8,12 @@ var port = process.env.PORT || 8080;
 // Handling Home url
 app.use(express.static(__dirname + '/public'))
 
+// Handling File Submit
+app.post('/get-file-data', upload.single('fileupload'), function(req,res){
+  
+  res.end(JSON.stringify({"File Size": req.file.size + ' bytes'}))
+    
+})
 
 
 
